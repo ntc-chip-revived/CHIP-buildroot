36e57d082a34e4313d5db3d8e6f13e7702127a7c
Alex Kaplan <kaplan2539@gmail.com>
2015-10-07 17:04:59 -0700

    board/nextthing/chip: fixed typo in upload_source.sh

--- board/nextthing/chip/upload_source.sh
+++ board/nextthing/chip/upload_source.sh
@@ -3,7 +3,7 @@
 BUILD_NUMBER=$1
 S3_DEST_BASE=s3://opensource.nextthing.co/chip/buildroot/next
 
-tar czf /tmp/CHIP-buildroot-build${BUILD_NUMBER}.tar.gz ../build; || exit $?
+tar czf /tmp/CHIP-buildroot-build${BUILD_NUMBER}.tar.gz ../build || exit $?
 
 s3cmd put --acl-public --no-guess-mime-type /tmp/CHIP-buildroot-build${BUILD_NUMBER}.tar.gz ${S3_DEST_BASE}/${BUILD_NUMBER}/build${BUILD_NUMBER}.tar.gz || exit $?
 
