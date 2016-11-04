0275c60b1b45a39eeeb2fef13729e2c89c94edaa
Wynter Woods <zerotri@gmail.com>
2016-11-04 12:36:18 -0700

    Blink leds added. Uboot env changed to support rapid iteration.

--- board/nextthing/chip/scripts/flash.sh
+++ board/nextthing/chip/scripts/flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BR_OUTPUT_DIR="${1}"
 sunxi-fel uboot ${BR_OUTPUT_DIR}/images/u-boot-sunxi-with-spl.bin
-sleep 10
+sleep 8
 fastboot -i 0x1f3a erase UBI
 fastboot -i 0x1f3a flash UBI ${BR_OUTPUT_DIR}/images/rootfs.ubi.sparse
 fastboot -i 0x1f3a continue -u
