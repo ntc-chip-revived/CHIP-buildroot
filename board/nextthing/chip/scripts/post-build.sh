a52146ca9cf6994766c74bfeac2984155aa4f299
Wynter Woods <zerotri@gmail.com>
2016-11-08 17:04:01 -0800

    Test script improvements. Add microphone status to UART2

--- board/nextthing/chip/scripts/post-build.sh
+++ board/nextthing/chip/scripts/post-build.sh
@@ -7,3 +7,9 @@ MKIMAGE=${HOST_DIR}/usr/bin/mkimage
 
 # ${MKIMAGE} -f ${BOARD_DIR}/bootimg.its ${BINARIES_DIR}/bootimg.itb
 
+cat <<-EOF >> ${TARGET_DIR}/etc/mdev.conf
+sd[a-z][0-9]* 0:0 0660 *(/opt/bin/automount $MDEV)
+mmcblk[0-9]p[0-9] 0:0 0660 *(/opt/bin/automount $MDEV)
+EOF
+
+echo "TERM=xterm" | tee -a ${TARGET_DIR}/etc/profile
