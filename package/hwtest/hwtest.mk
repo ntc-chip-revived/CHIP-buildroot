c81271cb23ea3200ca1ef352407bcf12e1d6293e
Jason Abele <jason@nextthing.co>
2016-11-17 16:39:34 -0800

    hwtest: default to ja/chippro-halt for test-on-boot
    Signed-off-by: Jason Abele <jason@nextthing.co>

--- package/hwtest/hwtest.mk
+++ package/hwtest/hwtest.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HWTEST_VERSION = chip/stable
+HWTEST_VERSION = ja/chippro-halt
 HWTEST_REPO_NAME=CHIP-hwtest
 HWTEST_SITE = https://github.com/NextThingCo/$(HWTEST_REPO_NAME)
 HWTEST_SITE_METHOD = git
